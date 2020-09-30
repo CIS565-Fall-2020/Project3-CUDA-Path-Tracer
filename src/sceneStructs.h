250ab7a482e1e93c3cd9ec2c810372b709a37874
giaosame <qzcqs@icloud.com>
2020-09-30 01:14:38 -0400

    add basic implementation of pathtrace()

--- src/sceneStructs.h
+++ src/sceneStructs.h
@@ -8,55 +8,55 @@
 #define BACKGROUND_COLOR (glm::vec3(0.0f))
 
 enum GeomType {
-    SPHERE,
-    CUBE,
+	SPHERE,
+	CUBE,
 };
 
 struct Ray {
-    glm::vec3 origin;
-    glm::vec3 direction;
+	glm::vec3 origin;
+	glm::vec3 direction;
 };
 
 struct Geom {
-    enum GeomType type;
-    int materialid;
-    glm::vec3 translation;
-    glm::vec3 rotation;
-    glm::vec3 scale;
-    glm::mat4 transform;
-    glm::mat4 inverseTransform;
-    glm::mat4 invTranspose;
+	enum GeomType type;
+	int materialid;
+	glm::vec3 translation;
+	glm::vec3 rotation;
+	glm::vec3 scale;
+	glm::mat4 transform;
+	glm::mat4 inverseTransform;
+	glm::mat4 invTranspose;
 };
 
 struct Material {
-    glm::vec3 color;
-    struct {
-        float exponent;
-        glm::vec3 color;
-    } specular;
-    float hasReflective;
-    float hasRefractive;
-    float indexOfRefraction;
-    float emittance;
+	glm::vec3 color;
+	struct {
+		float exponent;
+		glm::vec3 color;
+	} specular;
+	float hasReflective;
+	float hasRefractive;
+	float indexOfRefraction;
+	float emittance;
 };
 
 struct Camera {
-    glm::ivec2 resolution;
-    glm::vec3 position;
-    glm::vec3 lookAt;
-    glm::vec3 view;
-    glm::vec3 up;
-    glm::vec3 right;
-    glm::vec2 fov;
-    glm::vec2 pixelLength;
+	glm::ivec2 resolution;
+	glm::vec3 position;
+	glm::vec3 lookAt;
+	glm::vec3 view;
+	glm::vec3 up;
+	glm::vec3 right;
+	glm::vec2 fov;
+	glm::vec2 pixelLength;
 };
 
 struct RenderState {
-    Camera camera;
-    unsigned int iterations;
-    int traceDepth;
-    std::vector<glm::vec3> image;
-    std::string imageName;
+	Camera camera;
+	unsigned int iterations;
+	int traceDepth;
+	std::vector<glm::vec3> image;
+	std::string imageName;
 };
 
 struct PathSegment {
@@ -70,7 +70,24 @@ struct PathSegment {
 // 1) color contribution computation
 // 2) BSDF evaluation: generate a new ray
 struct ShadeableIntersection {
-  float t;
-  glm::vec3 surfaceNormal;
-  int materialId;
+	float t;
+	glm::vec3 surfaceNormal;
+	int materialId;
+};
+
+struct material_comp
+{
+	__host__ __device__ bool operator()(const ShadeableIntersection& isect1,
+		const ShadeableIntersection& isect2)
+	{
+		return isect1.materialId > isect2.materialId;
+	}
+};
+
+struct raytracing_continuing
+{
+	__host__ __device__ bool operator()(const PathSegment& segment)
+	{
+		return segment.remainingBounces > 0;
+	}
 };
