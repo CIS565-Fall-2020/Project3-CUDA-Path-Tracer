9c92a829fc9a0031aeee8fe92d63efd0330766f0
Sireesha Putcha <sireesha@seas.upenn.edu>
2020-10-01 01:30:39 -0400

    Added Output - Code is Working
    phew..

--- README.md
+++ README.md
@@ -3,11 +3,36 @@ CUDA Path Tracer
 
 **University of Pennsylvania, CIS 565: GPU Programming and Architecture, Project 3**
 
-* (TODO) YOUR NAME HERE
-* Tested on: (TODO) Windows 22, i7-2222 @ 2.22GHz 22GB, GTX 222 222MB (Moore 2222 Lab)
+## SIREESHA PUTCHA 
+	
+* <img src= "img/Logos/linkedin.png" alt = "LinkedIn" height = "30" width = "30">   [ LinkedIn ](https://www.linkedin.com/in/sireesha-putcha/)
 
-### (TODO: Your README)
+* <img src= "img/Logos/facebook.png" alt = "Fb" height = "30" width = "30">  [ Facebook ](https://www.facebook.com/sireesha.putcha98/)
 
-*DO NOT* leave the README to the last minute! It is a crucial part of the
-project, and we will not be able to grade you without a good README.
+* <img src= "img/Logos/chat.png" alt = "Portfolio" height = "30" width = "30">   [ Portfolio ](https://sites.google.com/view/sireeshaputcha/home)
 
+* <img src= "img/Logos/mail.png" alt = "Mail" height = "30" width = "30">  [ Mail ](sireesha@seas.upenn.edu)
+
+
+* Tested on personal computer - Microsoft Windows 10 Pro, 
+Processor : Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz, 2601 Mhz, 6 Core(s), 12 Logical Processor(s)
+ 
+GPU : NVIDIA GeForce RTX 2060
+
+## OUTPUT 
+
+1) Pure Diffuse 
+
+![Diffuse](img/diffuse_main.png)
+
+2) Pure Specular 
+
+![Specular](img/specular_main.png)
+
+## Bloopers 
+
+![bloop](img/blooper3.png)
+
+![bloop](img/blooper1.png)
+
+![bloop](img/blooper2.png)
--- src/interactions.h
+++ src/interactions.h
@@ -9,36 +9,91 @@
  */
 __host__ __device__
 glm::vec3 calculateRandomDirectionInHemisphere(
-        glm::vec3 normal, thrust::default_random_engine &rng) {
-    thrust::uniform_real_distribution<float> u01(0, 1);
+	glm::vec3 normal, thrust::default_random_engine& rng) {
 
-    float up = sqrt(u01(rng)); // cos(theta)
-    float over = sqrt(1 - up * up); // sin(theta)
-    float around = u01(rng) * TWO_PI;
+	thrust::uniform_real_distribution<float> u01(0, 1);
 
-    // Find a direction that is not the normal based off of whether or not the
-    // normal's components are all equal to sqrt(1/3) or whether or not at
-    // least one component is less than sqrt(1/3). Learned this trick from
-    // Peter Kutz.
+	float up = sqrt(u01(rng)); // cos(theta)
+	float over = sqrt(1 - up * up); // sin(theta)
+	float around = u01(rng) * TWO_PI;
 
-    glm::vec3 directionNotNormal;
-    if (abs(normal.x) < SQRT_OF_ONE_THIRD) {
-        directionNotNormal = glm::vec3(1, 0, 0);
-    } else if (abs(normal.y) < SQRT_OF_ONE_THIRD) {
-        directionNotNormal = glm::vec3(0, 1, 0);
-    } else {
-        directionNotNormal = glm::vec3(0, 0, 1);
-    }
+	// Find a direction that is not the normal based off of whether or not the
+	// normal's components are all equal to sqrt(1/3) or whether or not at
+	// least one component is less than sqrt(1/3). Learned this trick from
+	// Peter Kutz.
 
-    // Use not-normal direction to generate two perpendicular directions
-    glm::vec3 perpendicularDirection1 =
-        glm::normalize(glm::cross(normal, directionNotNormal));
-    glm::vec3 perpendicularDirection2 =
-        glm::normalize(glm::cross(normal, perpendicularDirection1));
+	glm::vec3 directionNotNormal;
+	if (abs(normal.x) < SQRT_OF_ONE_THIRD) {
+		directionNotNormal = glm::vec3(1, 0, 0);
+	}
+	else if (abs(normal.y) < SQRT_OF_ONE_THIRD) {
+		directionNotNormal = glm::vec3(0, 1, 0);
+	}
+	else {
+		directionNotNormal = glm::vec3(0, 0, 1);
+	}
 
-    return up * normal
-        + cos(around) * over * perpendicularDirection1
-        + sin(around) * over * perpendicularDirection2;
+	// Use not-normal direction to generate two perpendicular directions
+	glm::vec3 perpendicularDirection1 =
+		glm::normalize(glm::cross(normal, directionNotNormal));
+	glm::vec3 perpendicularDirection2 =
+		glm::normalize(glm::cross(normal, perpendicularDirection1));
+
+	return up * normal
+		+ cos(around) * over * perpendicularDirection1
+		+ sin(around) * over * perpendicularDirection2;
+}
+
+//Helper for Refraction 
+__host__ __device__ glm::vec3 getRefractedDirection(PathSegment& pathSegment,
+	glm::vec3 intersect,
+	glm::vec3 normal,
+	const Material& m,
+	thrust::default_random_engine&,
+	bool& flipDir)
+{
+	float eta = 1.f / m.indexOfRefraction;
+	float criticalAngle(0.f), theta(0.f);
+	//Set the index of refraction and check for Total Internal Reflection 
+	glm::vec3 rayDirection = pathSegment.ray.direction;
+	glm::vec3 refractedDir(0.f);
+	float cos_theta = glm::dot(rayDirection, normal);
+
+	//Entering the object
+	if (cos_theta <= 0.f)
+	{
+		theta = acos(glm::dot(-rayDirection, normal));
+		criticalAngle = asin(eta);
+		//Total internal reflection? 
+		if (eta < 1.f && theta > criticalAngle)
+		{
+			refractedDir = glm::reflect(rayDirection, -normal);
+			flipDir = false;
+		}
+		else
+		{
+			refractedDir = glm::refract(rayDirection, normal, eta);
+			flipDir = true;
+		}
+	}
+	//Exiting the object 
+	else
+	{
+		theta = acos(glm::dot(rayDirection, normal));
+		criticalAngle = asin(m.indexOfRefraction);
+		//Total internal reflection? 
+		if (m.indexOfRefraction < 1.f && theta > criticalAngle)
+		{
+			refractedDir = glm::reflect(rayDirection, -normal);
+			flipDir = true;
+		}
+		else
+		{
+			refractedDir = glm::refract(rayDirection, -normal, eta);
+			flipDir = false;
+		}
+	}
+	return refractedDir;
 }
 
 /**
@@ -47,11 +102,11 @@ glm::vec3 calculateRandomDirectionInHemisphere(
  * A perfect specular surface scatters in the reflected ray direction.
  * In order to apply multiple effects to one surface, probabilistically choose
  * between them.
- * 
+ *
  * The visual effect you want is to straight-up add the diffuse and specular
  * components. You can do this in a few ways. This logic also applies to
  * combining other types of materias (such as refractive).
- * 
+ *
  * - Always take an even (50/50) split between a each effect (a diffuse bounce
  *   and a specular bounce), but divide the resulting color of either branch
  *   by its probability (0.5), to counteract the chance (0.5) of the branch
@@ -68,12 +123,46 @@ glm::vec3 calculateRandomDirectionInHemisphere(
  */
 __host__ __device__
 void scatterRay(
-		PathSegment & pathSegment,
-        glm::vec3 intersect,
-        glm::vec3 normal,
-        const Material &m,
-        thrust::default_random_engine &rng) {
-    // TODO: implement this.
-    // A basic implementation of pure-diffuse shading will just call the
-    // calculateRandomDirectionInHemisphere defined above.
+	PathSegment& pathSegment,
+	glm::vec3 intersect,
+	glm::vec3 normal,
+	const Material& m,
+	thrust::default_random_engine& rng) {
+	// TODO: implement this.
+	// A basic implementation of pure-diffuse shading will just call the
+	// calculateRandomDirectionInHemisphere defined above.
+
+	thrust::uniform_real_distribution<float> u01(0, 1);
+	float randDist = u01(rng);
+
+	//Pure diffuse shading 
+	if (m.hasReflective == 0.f && m.hasRefractive == 0.f)
+	{
+		glm::vec3 randDirInHemisphere = calculateRandomDirectionInHemisphere(normal, rng);
+		pathSegment.ray.origin = intersect + (EPSILON * normal);
+		pathSegment.ray.direction = glm::normalize(randDirInHemisphere);
+		pathSegment.color *= m.color;
+	}
+	//Reflective surface 
+	else if (m.hasReflective >= randDist && m.hasRefractive == 0.f)
+	{
+		glm::vec3 reflectedDir = glm::reflect(pathSegment.ray.direction, normal); //For the incident vector I and surface orientation N, 
+																				  //returns the reflection direction
+		pathSegment.ray.origin = intersect + (EPSILON * normal);
+		pathSegment.ray.direction = glm::normalize(reflectedDir);
+		pathSegment.color *= m.specular.color;
+	}
+	//Refractive surface 
+	else {
+		bool flipDir(0);
+		pathSegment.ray.direction = glm::normalize(getRefractedDirection(pathSegment, intersect, normal, m, rng, flipDir));
+		if (flipDir) {
+			pathSegment.ray.origin = intersect + (EPSILON * -normal);
+		}
+		else {
+			pathSegment.ray.origin = intersect + (EPSILON * normal);
+		}
+		pathSegment.color *= m.specular.color;
+	}
+
 }
