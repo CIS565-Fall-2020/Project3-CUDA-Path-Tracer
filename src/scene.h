8039f845111c8fb12cc5baa2c14975e9ee25ca72
Thy Tran (Tea) <tatran@seas.upenn.edu>
2020-10-04 21:52:20 -0400

    added triangle struct

--- src/scene.h
+++ src/scene.h
@@ -21,6 +21,7 @@ public:
     ~Scene();
 
     std::vector<Geom> geoms;
+    std::vector<Triangle> tris;
     std::vector<Material> materials;
     RenderState state;
 };
--- src/sceneStructs.h
+++ src/sceneStructs.h
@@ -8,55 +8,67 @@
 #define BACKGROUND_COLOR (glm::vec3(0.0f))
 
 enum GeomType {
-    SPHERE,
-    CUBE,
+	SPHERE,
+	CUBE,
+	OBJ
 };
 
 struct Ray {
-    glm::vec3 origin;
-    glm::vec3 direction;
+	glm::vec3 origin;
+	glm::vec3 direction;
 };
 
 struct Geom {
-    enum GeomType type;
-    int materialid;
-    glm::vec3 translation;
-    glm::vec3 rotation;
-    glm::vec3 scale;
-    glm::mat4 transform;
-    glm::mat4 inverseTransform;
-    glm::mat4 invTranspose;
+	enum GeomType type;
+	int materialid;
+	glm::vec3 translation;
+	glm::vec3 rotation;
+	glm::vec3 scale;
+	glm::mat4 transform;
+	glm::mat4 inverseTransform;
+	glm::mat4 invTranspose;
+	int triangleIdxStar = -1;
+	int triangleIdxEnd = -1;
+};
+
+struct Triangle {
+	glm::vec3 v1;
+	glm::vec3 v2;
+	glm::vec3 v3;
+	glm::vec3 n1;
+	glm::vec3 n2;
+	glm::vec3 n3;
 };
 
 struct Material {
-    glm::vec3 color;
-    struct {
-        float exponent;
-        glm::vec3 color;
-    } specular;
-    float hasReflective;
-    float hasRefractive;
-    float indexOfRefraction;
-    float emittance;
+	glm::vec3 color;
+	struct {
+		float exponent;
+		glm::vec3 color;
+	} specular;
+	float hasReflective;
+	float hasRefractive;
+	float indexOfRefraction;
+	float emittance;
 };
 
 struct Camera {
-    glm::ivec2 resolution;
-    glm::vec3 position;
-    glm::vec3 lookAt;
-    glm::vec3 view;
-    glm::vec3 up;
-    glm::vec3 right;
-    glm::vec2 fov;
-    glm::vec2 pixelLength;
+	glm::ivec2 resolution;
+	glm::vec3 position;
+	glm::vec3 lookAt;
+	glm::vec3 view;
+	glm::vec3 up;
+	glm::vec3 right;
+	glm::vec2 fov;
+	glm::vec2 pixelLength;
 };
 
 struct RenderState {
-    Camera camera;
-    unsigned int iterations;
-    int traceDepth;
-    std::vector<glm::vec3> image;
-    std::string imageName;
+	Camera camera;
+	unsigned int iterations;
+	int traceDepth;
+	std::vector<glm::vec3> image;
+	std::string imageName;
 };
 
 struct PathSegment {
@@ -70,7 +82,7 @@ struct PathSegment {
 // 1) color contribution computation
 // 2) BSDF evaluation: generate a new ray
 struct ShadeableIntersection {
-  float t;
-  glm::vec3 surfaceNormal;
-  int materialId;
+	float t;
+	glm::vec3 surfaceNormal;
+	int materialId;
 };
