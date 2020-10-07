f91898cf6b16a5f2ed1b5d3c566091541dba7373
HAN YAN <hyan99@park.key.seas.upenn.edu>
2020-10-07 16:28:37 -0400

    box-tri intersection init

--- src/octree.cpp
+++ src/octree.cpp
@@ -1,4 +1,5 @@
 #include "octree.h"
+#include <algorithm>
 
 OctreeNode::OctreeNode()
 {}
@@ -10,6 +11,34 @@ OctreeNode::OctreeNode(glm::vec3 &v0, glm::vec3 &v1)
 }
 
 bool OctreeNode::intersectTriangle(const Geom &geom) const {
+	glm::vec3 vt0 = geom.v0 - center;
+	glm::vec3 vt1 = geom.v1 - center;
+	glm::vec3 vt2 = geom.v2 - center;
 
+	glm::vec3 e0 = vt1 - vt0;
+	glm::vec3 e1 = vt2 - vt1;
+	glm::vec3 e2 = vt0 - vt2;
+
+	glm::vec3 n0 = glm::vec3(1.f, 0.f, 0.f);
+	glm::vec3 n1 = glm::vec3(0.f, 1.f, 0.f);
+	glm::vec3 n2 = glm::vec3(0.f, 0.f, 1.f);
+
+	float dx = center[0] - bp0[0];
+	float dy = center[1] - bp0[1];
+	float dz = center[2] - bp0[2];
+
+	for (const auto &v : { e0, e1, e2 }) {
+		for (const auto &u : { n0, n1, n2 }) {
+			glm::vec3 axis = glm::cross(u, v);
+			float p0 = glm::dot(vt0, axis);
+			float p1 = glm::dot(vt1, axis);
+			float p2 = glm::dot(vt2, axis);
+			float r = dx * abs(glm::dot(n0, axis)) + dy * abs(glm::dot(n1, axis)) + dz * abs(glm::dot(n2, axis));
+			if (max(max(p0, p1), p2) > r || min(min(p0, p1), p2) < -r) {
+				// can be separated
+				return false;
+			}
+		}
+	}
 }
 
